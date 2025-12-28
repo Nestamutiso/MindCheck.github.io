11a5b3802c934ef2a249e5a64053bebb279703b9
Nestamutiso <enochmulwa2@gmail.com>
2025-12-28 20:24:46 +0000

    add android build alongside pages deploy

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nesta.mindcheck',
+  appName: 'MindCheck',
+  webDir: 'dist'
+};
+
+export default config;
